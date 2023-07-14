814760a147d4ae5793a02a3b811f5b99f454a651
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-07-14 18:24:20 +0300

    add-button

--- app/components/main/Main.jsx
+++ app/components/main/Main.jsx
@@ -2,15 +2,23 @@ import style from "./main.module.scss";
 import Container from "@/app/share/container/Container";
 
 const Main = () => {
+  const location = "https://goo.gl/maps/ZJZfS61p7HjnXdkG7";
 
   return (
     <section className={style.section}>
       <Container>
         <div className={style.wrapper}>
-         <h1 className={style.title}>
+          <h1 className={style.title}>
             Готові букети для придбання прямо зараз
-         </h1>
-         <button>ЛОКАЦІЇ</button>
+          </h1>
+          <a
+            href={location}
+            target="_blank"
+            rel="noreferrer"
+            className={style.buttonLocation}
+          >
+            локації
+          </a>
         </div>
       </Container>
     </section>
