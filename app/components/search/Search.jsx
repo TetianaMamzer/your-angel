3a9dc3ae539f17b284196af59d1a1a62cec0988d
Ivan Alimenko <ivan.alimenko@ukr.net>
2023-07-21 23:53:42 +0300

    section-init

---
+++ app/components/search/Search.jsx
@@ -0,0 +1,22 @@
+import style from "./search.module.scss";
+import Container from "@/app/share/container/Container";
+
+const Search = () => {
+
+  return (
+    <section className={style.section}>
+      <Container>
+        <h2 className={style.title}>Швидкий пошук</h2>
+        <p className={style.text}>
+          Діє система знижок 5%,10%,15% в залежності від загальної кількості
+          замовлення однакових букетів
+        </p>
+        <ul className={style.list}>
+          <li></li>
+        </ul>
+      </Container>
+    </section>
+  );
+};
+
+export default Search;
--- app/page.js
+++ app/page.js
@@ -3,12 +3,14 @@ import styles from './page.module.scss';
 import Faq from './components/faq/Faq';
 import Lesson from './components/lesson/Lesson';
 import Main from './components/main/Main';
+import Search from './components/search/Search'
 import Delivery from './components/delivery/Delivery';
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <Main />
+      <Search />
       <Lesson />
       <Faq />
       <Delivery/>
