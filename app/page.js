93a1bfaaf2a997bc4ee0337a2d7df9c296a89620
E-gap <106596181+E-gap@users.noreply.github.com>
2023-07-24 15:52:59 +0300

    delete comments

--- app/page.js
+++ app/page.js
@@ -7,19 +7,8 @@ import Main from "./components/main/Main";
 import Search from "./components/search/Search";
 import Delivery from "./components/delivery/Delivery";
 import ButtonToTop from "./components/buttonToTop/ButtonToTop";
-// import { useEffect, useState } from "react";
 
 export default function Home() {
-  // const [fromTop, setFromTop] = useState(0);
-
-  /* useEffect(() => {
-    window.addEventListener("scroll", listener);
-    return () => window.removeEventListener("scroll", listener);
-  }, []); */
-
-  /* function listener() {
-    setFromTop(window.pageYOffset);
-  } */
   return (
     <main className={styles.main}>
       <ButtonToTop />
