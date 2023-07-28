4b6e377f4fcbedc1deb5cdc71868c7de39091c95
E-gap <106596181+E-gap@users.noreply.github.com>
2023-07-28 14:55:01 +0300

    styles callback

--- app/components/callBackMenu/CallBackMenu.jsx
+++ app/components/callBackMenu/CallBackMenu.jsx
@@ -1,14 +1,73 @@
+"use client";
+import { Formik, Form, Field, ErrorMessage } from "formik";
+import * as Yup from "yup";
+
 import styles from "./callBackMenu.module.scss";
 
 const CallBackMenu = () => {
-  /* const handleClickOnLink = (e) => {
-    if (e.target.hasAttribute("href")) {
-      setShowModal(false);
-    }
-  }; */
+  const CallBackSchema = Yup.object().shape({
+    name: Yup.string().required("Будь ласка, введіть Ваше ім'я"),
+    phone: Yup.string().required("Будь ласка, введіть Ваш контактний телефон"),
+    email: Yup.string(),
+  });
+
+  const submitForm = (values, actions) => {
+    actions.resetForm();
+    console.log(values);
+  };
   return (
     <div className={styles.callBackMenu}>
-      <h1 className={styles.headline}>ВВЕДІТЬ НЕОБХІДНІ ДАНІ І З ВАМИ ЗВ’ЯЖУТЬСЯ ЗА 10 ХВИЛИН!</h1>
+      <h1 className={styles.headline}>
+        ВВЕДІТЬ НЕОБХІДНІ ДАНІ І З ВАМИ ЗВ’ЯЖУТЬСЯ ЗА 10 ХВИЛИН!
+      </h1>
+      <Formik
+        initialValues={{ name: "", phone: "", email: "" }}
+        validationSchema={CallBackSchema}
+        onSubmit={submitForm}
+      >
+        <Form className={styles.form}>
+          <label className={styles.label}>
+            <Field name="name" className={styles.input} placeholder="ІМ'Я" />
+            <p className={styles.nessesaryField}>*це обов’язкове поле</p>
+            {/* <ErrorMessage
+              name="name"
+              render={(message) => (
+                <div className={styles.errorValidation}>{message}</div>
+              )}
+            /> */}
+          </label>
+          <label className={styles.label}>
+            <Field
+              name="phone"
+              className={`${styles.input} ${styles.inputPhone}`}
+              placeholder="ТЕЛЕФОН (098 76 54 321)"
+            />
+            <p className={styles.nessesaryField}>*це обов’язкове поле</p>
+            {/* <ErrorMessage
+              name="phone"
+              render={(message) => (
+                <div className={styles.errorValidation}>{message}</div>
+              )}
+            /> */}
+          </label>
+          <label className={styles.label}>
+            <Field
+              name="email"
+              className={styles.input}
+              placeholder="ЕЛ. ПОШТА"
+            />
+            {/* <ErrorMessage
+              name="email"
+              render={(message) => (
+                <div className={styles.errorValidation}>{message}</div>
+              )}
+            /> */}
+          </label>
+          <button type="submit" className={styles.submit}>
+            ЗАМОВИТИ
+          </button>
+        </Form>
+      </Formik>
     </div>
   );
 };
