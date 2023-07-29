44a8be04b7616556797181cbe2b425a5de881f32
E-gap <106596181+E-gap@users.noreply.github.com>
2023-07-29 11:44:59 +0300

    fix styles mobile menu

---
+++ app/components/afterCallBack/AfterCallBack.jsx
@@ -0,0 +1,12 @@
+import styles from "./afterCallBack.module.scss";
+
+const AfterCallBack = () => {
+  return (
+    <p className={styles.message}>
+      ВАША ЗАЯВКА ПРИЙНЯТА! ВАМ ЗАТЕЛЕФОНУЮТЬ ПРОТЯГОМ 10 ХВИЛИН В РОБОЧІ
+      ГОДИНИ! ГАРНОГО ДНЯ!
+    </p>
+  );
+};
+
+export default AfterCallBack;
